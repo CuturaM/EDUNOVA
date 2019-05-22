c4ed1c0ad3053a2a8b148c1a9d07409f7db0d91c
CuturaM <50337345+CuturaM@users.noreply.github.com>
2019-05-22 22:53:47 +0200

    Dodavanje FK i podataka u bazu

--- SQL/Skripta Doktorska ordinacija/skriptadoktorskaordinacija.sql
+++ SQL/Skripta Doktorska ordinacija/skriptadoktorskaordinacija.sql
@@ -38,3 +38,43 @@ sifra int not null primary key auto_increment,
 osoba int not null,
 iban varchar(32)
 );
+
+
+alter table lijecenje add foreign key (pacijent) references osoba(sifra);
+
+alter table pacijent add foreign key (osoba) references osoba(sifra);
+
+alter table sestra add foreign key (osoba) references osoba(sifra);
+
+
+insert into osoba
+(ime,prezime,oib,email) values
+/* 1 */
+('Petra','Petrić','11111111111','petra.petric@gmail.com'),
+/* 2 */
+('Ivan','Ivanović','22222222222','ivan.ivanovic@gmail.com'),
+/* 3 */
+('Marko','Markić','33333333333','marko.markic@gmail.com'),
+/* 4 */ 
+('Filipa','Filipović','44444444444','filipa.filipovic@gmail.com');
+
+insert into sestra 
+(osoba,iban) values
+/* 1 */
+(1,'HR8624840081102131080'),
+/* 2 */
+(4,'HR8624840081102336110');
+
+insert into pacijent
+(osoba) values
+/* 1 */
+(2),
+/* 2 */
+(3);
+
+insert into lijecenje
+(pacijent) values
+/* 1 */
+(2),
+/* 2 */ 
+(3);
