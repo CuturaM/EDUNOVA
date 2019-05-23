b168d6dc9e5e43a4fc3ad6558360c82053007829
CuturaM <50337345+CuturaM@users.noreply.github.com>
2019-05-23 11:07:13 +0200

    Promjene strukture baze dodavanje Fk i unos podataka

--- SQL/Skripta Samostan/skriptasamostan.sql
+++ SQL/Skripta Samostan/skriptasamostan.sql
@@ -1,6 +1,6 @@
 /* 
 Ova datoteka se nalazi na:
-mysql -ucuturam -p --default_character_set=utf8 < "d:\skriptasamostan1862"
+mysql -ucuturam -p --default_character_set=utf8 < "d:\skriptasamostan1862.sql"
 */
 
 
@@ -15,23 +15,50 @@ ime varchar(50) not null,
 prezime varchar(50) not null,
 iban varchar(32) not null,
 email varchar(100) not null,
-nadredjenisvecenik int not null
+nadredjenisvecenik int
 );
 
 
 create table posao(
 sifra int not null primary key auto_increment,
 svecenik int not null,
-trajanje int,
+trajanje varchar(50),
+naziv varchar(100) not null,
 vrijeme datetime not null
 );
 
 
-create table vrijeme(
-sifra int not null primary key auto_increment,
-posao int not null,
-svecenik int not null,
-pocetak datetime
-);
+
+alter table svecenik add foreign key (nadredjenisvecenik) references svecenik(sifra);
+
+alter table posao add foreign key (svecenik) references svecenik(sifra);
+
+
+insert into svecenik 
+(oib,ime,prezime,iban,email,nadredjenisvecenik) values
+/* 1 */
+('32855883549','Ante','Starčević','HR7524020065253647397','ante.starcevic@gmail.com',null),
+/* 2 */ 
+('58151183615','Marko','Marulić','HR0924020062948126686','marko.marulic@gmail.com',1),
+/* 3 */ 
+('07304390318','Josip','Jelačić','HR7625000096956472614','josip.jelacic@gmail.com',1),
+/* 4 */
+('78565773717','Juraj','Dobrila','HR2023400091222257584','juraj.dobrila@gmail.com',1),
+/* 5 */
+('94023103808','Ivan','Gundulić','HR8325000095764397293','ivan.gundulic@gmail.com',1);
+
+insert into posao
+(svecenik,trajanje,naziv,vrijeme) values
+/* 1 */
+(2,'30 min.','Misa','2019-05-23 12:00:00'),
+/* 2 */
+(3,'40 min.','Misa','2019-05-23 14:30:00'),
+/* 3 */
+(4,'15 min.','Ispovjed','2019-05-23 17:15:00'),
+/* 4 */
+(5,'15 min.','Ispovjed','2019-05-23 17:15:00');
+
+
+
 
 
