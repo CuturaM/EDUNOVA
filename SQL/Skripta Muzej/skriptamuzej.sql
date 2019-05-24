4ff341ed67bd041a44c0538a6e5a28f6ae8b503e
CuturaM <50337345+CuturaM@users.noreply.github.com>
2019-05-24 11:47:02 +0200

    Promjene strukture baze dodavanje Fk i unos podataka

--- SQL/Skripta Muzej/skriptamuzej.sql
+++ SQL/Skripta Muzej/skriptamuzej.sql
@@ -13,6 +13,7 @@ sifra int not null primary key auto_increment,
 naziv varchar(100) not null,
 cijenaulaznice decimal(18,2) not null,
 kustos int not null,
+dijelo int not null,
 sponzor int not null
 );
 
@@ -20,8 +21,7 @@ sponzor int not null
 create table dijelo(
 sifra int not null primary key auto_increment,
 naziv varchar(100) not null,
-minutaza int not null,
-izlozba int not null
+minutaza varchar(50)
 );
 
 
@@ -48,4 +48,57 @@ ime varchar(50) not null,
 prezime varchar(50) not null,
 email varchar(100)
 );
+
+alter table kustos add foreign key (osoba) references osoba(sifra);
+alter table sponzor add foreign key (osoba) references osoba(sifra);
+
+alter table izlozba add foreign key (kustos) references kustos(sifra);
+alter table izlozba add foreign key (dijelo) references dijelo(sifra);
+alter table izlozba add foreign key (sponzor) references sponzor(sifra);
+
+insert into osoba
+(oib,ime,prezime,email) values
+/* 1 */ 
+('25564306144','Ante','Starčević','ante.starcevic@gmail.com'),
+/* 2 */
+('85266523210','Stjepan','Radić','stjepan.radic@gmail.com'),
+/* 3 */
+('55515186608','Marko','Marulić','marko.marulic@gmail.com'),
+/* 4 */
+('97379984083','Josip','Jelačić','josip.jelacic@gmail.com'),
+/* 5 */
+('38156367435','Ivan','Gundulić','ivan.gundulic@gmail.com');
+
+insert into kustos
+(osoba,iban) values
+/* 1 */
+(1,'HR8023400097215226419'),
+/* 2 */
+(2,'HR5524020069459566787'),
+/* 3 */
+(3,'HR1924020064181138126');
+
+insert into sponzor
+(firma,nazivfirme,osoba,iznosdonacije) values
+/* 1 */
+(true,'Ericsson',null,3000.00),
+/* 2 */
+(false,null,5,2000.00);
+
+insert into dijelo
+(naziv,minutaza) values
+/* 1 */
+('ACT 1','100 min.'),
+/* 2 */
+('ACT 2','60 min.'),
+/* 3 */
+('ACT 3','120 min.');
+
+
+insert into izlozba
+(naziv,cijenaulaznice,kustos,dijelo,sponzor) values
+/* 1 */
+('Izložba Prva',50.00,1,1,1),
+/* 2 */
+('Izložba Druga',60.00,2,2,2);
 
