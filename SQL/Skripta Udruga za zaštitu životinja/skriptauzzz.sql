652e3dfd57e5929874e2bbfb1190c030a85395d3
CuturaM <50337345+CuturaM@users.noreply.github.com>
2019-05-24 13:56:51 +0200

    Dodavanje FK i podataka u bazu

--- SQL/Skripta Udruga za zaštitu životinja/skriptauzzz.sql
+++ SQL/Skripta Udruga za zaštitu životinja/skriptauzzz.sql
@@ -1,6 +1,6 @@
 /* 
 Ova datoteka se nalazi na:
-mysql -ucuturam -p --default_character_set=utf8 < "d:\skriptauzz.sql"
+mysql -ucuturam -p --default_character_set=utf8 < "d:\skriptauzzz.sql"
 */
 
 drop database if exists uzzz1862;
@@ -31,4 +31,31 @@ sifra int not null primary key auto_increment,
 povrsina varchar(50) not null,
 grijanje boolean not null
 );
+
+
+
+alter table sticenik add foreign key (zaposlenik) references zaposlenik(sifra);
+alter table sticenik add foreign key (prostor) references prostor(sifra);
+
+
+insert into prostor 
+(povrsina,grijanje) values
+/* 1 */
+('62 m2',true),
+/* 2 */
+('40 m2',false);
+
+insert into zaposlenik
+(oib,ime,prezime,iban,email) values
+/* 1 */
+('25564306144','Ante','Starčević','HR8023400097215226419','ante.starcevic@gmail.com'),
+/* 2 */
+('85266523210','Stjepan','Radić','HR5524020069459566787','stjepan.radic@gmail.com');
+
+insert into sticenik
+(ime,prezime,zaposlenik,prostor) values
+/* 1 */
+('Oz',null,1,1),
+/* 2 */
+('Hasan','Ferhatović',2,2);
 
