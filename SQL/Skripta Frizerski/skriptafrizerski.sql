b2436c0492d21751a545c1bcb64366a5cb1da9c4
CuturaM <50337345+CuturaM@users.noreply.github.com>
2019-05-25 11:03:41 +0200

    Dodavanje FK i podataka u bazu

--- SQL/Skripta Frizerski/skriptafrizerski.sql
+++ SQL/Skripta Frizerski/skriptafrizerski.sql
@@ -33,3 +33,33 @@ usluga varchar(100) not null,
 cijena decimal(18,2) not null,
 datumpocetka datetime not null
 );
+
+
+
+alter table korisnik add foreign key (zaposlenik) references zaposlenik(sifra);
+alter table korisnik add foreign key (posjet) references posjet(sifra);
+
+insert into zaposlenik
+(oib,ime,prezime,iban,email) values
+/* 1 */
+('25564306144','Ante','Starčević','HR8023400097215226419','ante.starcevic@gmail.com'),
+/* 2 */
+('85266523210','Stjepan','Radić','HR5524020069459566787',null);
+
+insert into posjet
+(usluga,cijena,datumpocetka) values
+/* 1 */
+('Šišanje',35.00,'2019-05-25 12:00:00'),
+/* 2 */
+('Farbanje',150.00,'2019-03-24 10:30:00'),
+/* 3 */
+('Šišanje',35.00,'2019-02-14 17:00:00');
+
+insert into korisnik 
+(ime,prezime,brojtelefona,zaposlenik,posjet) values
+/* 1 */
+('Marko','Marulić',null,1,1),
+/* 2 */
+('Josip','Jelačić','00385956003232',2,2),
+/* 3 */
+('Marko','Marulić',null,1,3);
