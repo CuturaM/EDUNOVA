099a73287797b0b96c9d953f50b5804c230997cd
CuturaM <50337345+CuturaM@users.noreply.github.com>
2019-05-25 11:30:41 +0200

    Dodavanje FK i podataka u bazu

--- SQL/Skripta Dječiji vrtić/skriptadjecijivrtic.sql
+++ SQL/Skripta Dječiji vrtić/skriptadjecijivrtic.sql
@@ -40,3 +40,45 @@ oib char(11) not null,
 ime varchar(50) not null,
 prezime varchar(50) not null
 );
+
+
+alter table odgajatelj add foreign key (osoba) references osoba(sifra);
+
+alter table odgojnaskupina add foreign key (odgajatelj) references odgajatelj(sifra);
+
+alter table dijete add foreign key (osoba) references osoba(sifra);
+alter table dijete add foreign key (odgojnaskupina) references odgojnaskupina(sifra);
+
+insert into osoba
+(oib,ime,prezime) values
+/* 1 */
+('32855883549','Ante','Starčević'),
+/* 2 */
+('85266523210','Stjepan','Radić'),
+/* 3 */
+('55515186608','Josip','Jelačić'),
+/* 4 */
+('97379984083','Marko','Marulić'),
+/* 5 */
+('38156367435','Ivan','Gundulić');
+
+insert into odgajatelj
+(osoba,iban,strucnasprema,email) values
+/* 1 */
+(1,'HR8023400097215226419','SSS','ante.starcevic@gmail.com');
+
+insert into odgojnaskupina
+(naziv,cijena,trajanje,odgajatelj) values
+/* 1 */
+('A',1500.00,200,1);
+
+insert into dijete
+(osoba,kontakt,spol,odgojnaskupina) values
+/* 1 */
+(2,'00385988887777','Muško',1),
+/* 2 */
+(3,'josip.jelacic@gmail.com','Muško',1),
+/* 3 */
+(4,'00385950005252','Muško',1),
+/* 4 */ 
+(5,'00386950054444','Muško',1);
