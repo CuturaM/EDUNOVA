f8f3f9f7d48aaeafd629dee40e32c8fd181b36a9
CuturaM <50337345+CuturaM@users.noreply.github.com>
2019-05-25 23:31:08 +0200

    Dodavanje FK i podataka u bazu

--- SQL/Skripta Trgovina/skriptatrgovina.sql
+++ SQL/Skripta Trgovina/skriptatrgovina.sql
@@ -26,3 +26,35 @@ sifra int not null primary key auto_increment,
 naziv varchar(100) not null,
 cijena decimal(18,2) not null
 );
+
+
+
+alter table stavka add foreign key (racun) references racun(sifra);
+alter table stavka add foreign key (proizvod) references proizvod(sifra);
+
+
+
+insert into proizvod 
+(naziv,cijena) values
+/* 1 */
+('Marlboro',30.00),
+/* 2 */
+('Pan',9.80),
+/* 3 */
+('Karlovačko',9.99);
+
+insert into racun
+(datum,kupac) values
+/* 1 */
+('2018-12-02',null),
+/* 2 */
+('2019-01-15','Romos-int d.o.o');
+
+insert into stavka
+(racun,proizvod,kolicina) values
+/* 1 */
+(1,1,15.000),
+/* 2 */
+(1,3,10.000),
+/* 3 */
+(2,2,900.000);
