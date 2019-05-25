7008a161b561466d284ef8c44ece00b5b3bae9e7
CuturaM <50337345+CuturaM@users.noreply.github.com>
2019-05-25 22:28:58 +0200

    Dodavanje FK i podataka u bazu

--- SQL/Skripta HNL/skriptahnl.sql
+++ SQL/Skripta HNL/skriptahnl.sql
@@ -22,3 +22,24 @@ sifra int not null primary key auto_increment,
 naziv varchar(50) not null,
 mjesto varchar(50) not null
 );
+
+
+
+alter table utakmica add foreign key (domacin) references momcad(sifra),
+alter table utakmica add foreign key (gost) references momcad(sifra);
+
+
+
+insert into momcad 
+(naziv,mjesto) values
+/* 1 */
+('Dinamo','Zagreb'),
+/* 2 */
+('Hajduk','Split');
+
+insert into utakmica
+(datum,stadion,domacin,gost,rezultat) values
+/* 1 */
+('2019-04-11','Maksimir',1,2,10.00),
+/* 2 */
+('2019-05-20','Poljud',2,1,00.09);
