92937850f7b6d405ebab2298bcef22d9245ce495
Marin Čutura <w.cutura1990@gmail.com>
2019-06-06 20:54:49 +0200

    Dodavanje artikala TOPLI NAPITCI

--- ELEONORA/skriptaeleonora.sql
+++ ELEONORA/skriptaeleonora.sql
@@ -91,24 +91,38 @@ alter table racun add foreign key (kupac) references kupac(sifra);
 insert into artikl
 (naziv,zaliha,prodajnacijena,nabavnacijena,normativ,kategorija,skladiste,slika) values
 /* 1 */
-('COCA COLA 0.25L',240.000,15.00,3.09,1.000,'BEZALKOHOLNA PIĆA','ŠANK1','https://drive.google.com/open?id=15vY9Xn-_PXikAzKeVFcrIWwuYrYN5BeG'),
-/* 2 *//* 10 */
-('FANTA 0.25L',240.000,15.00,3.09,1.000,'BEZALKOHOLNA PIĆA','ŠANK1','https://drive.google.com/open?id=1zQP5JnwCbUo9sTgM7OLLlUJ4Dyp2AsN-'),
+('ESPRESSO',5.000,8.00,0.89,0.007,'TOPLI NAPITCI','ŠANK1','https://drive.google.com/open?id=1a-H6zom5oyedSN7Teu3At8t2W3UFj_74'),
+/* 2 */
+('KAVA S MLIJEKOM',0.000,9.00,0.89,0.007,'TOPLI NAPITCI','ŠANK1','https://drive.google.com/open?id=1b8RGnadP3jk6wSWAWgm94wcGwQD_0vEa'),
 /* 3 */
-('SPRITE 0.25L',240.000,15.00,3.09,1.000,'BEZALKOHOLNA PIĆA','ŠANK1','https://drive.google.com/open?id=1C6M7rIXE-_WHDmkW1F7TxVMFHuSKjv-l'),
+('KAVA MLIJEKO PRODUŽENA',0.000,10.00,0.89,0.007,'TOPLI NAPITCI','ŠANK1','https://drive.google.com/open?id=11y7DiSGXSvv5cRHGpRr9xF4FSswMJnk2'),
 /* 4 */
-('SCHWEPPES 0.25L',240.000,15.00,3.20,1.000,'BEZALKOHOLNA PIĆA','ŠANK1','https://drive.google.com/open?id=1grQiwBmWRgEAQW09L7a2GS9jgyu2j7BM'),
+('CAPPUCCINO',0.000,10.00,0.89,0.007,'TOPLI NAPITCI','ŠANK1','https://drive.google.com/open?id=1wkobzO4NXots4FvDXeJmmC4eQQiO67p4'),
 /* 5 */
-('PRIRODNI SOK CAPPY 0.20L',200.000,15.00,3.50,1.000,'BEZALKOHOLNA PIĆA','ŠANK1','https://drive.google.com/open?id=1a8BVXogDk2dQQnXlfK8JlTenLlNxs62J'),
-/* 6 */
-('LEDENI ČAJ 0.20L',240.000,15.00,2.30,1.000,'BEZALKOHOLNA PIĆA','ŠANK1','https://drive.google.com/open?id=1mWwYs-avXsl35AD6sKFSMG0malsL3x3Z'),
+('BIJELA KAVA',0.000,12.00,0.89,0.007,'TOPLI NAPITCI','ŠANK1','https://drive.google.com/open?id=1kKi1HzkitR6IcsubHr7igkvjUP8sZ5VB'),
+/* 6*/
+('NESCAFE',250.000,12.00,1.37,1.000,'TOPLI NAPITCI','ŠANK1','https://drive.google.com/open?id=1ldfwjRPD-gqTse3NZ6lvZ9byyaOwsniX'),
 /* 7 */
-('ORANGINA 0.25L',240.000,16.00,4.39,1.000,'BEZALKOHOLNA PIĆA','ŠANK1','https://drive.google.com/open?id=1jQilEO4pQdYuRzXc0TcpXgXZ9DkFt1xW'),
+('ČAJ',400.000,10.00,0.40,1.000,'TOPLI NAPITCI','ŠANK1','https://drive.google.com/open?id=1ldfwjRPD-gqTse3NZ6lvZ9byyaOwsniX'),
 /* 8 */
-('CEDEVITA',400.000,11.00,2.11,1.000,'BEZALKOHOLNA PIĆA','ŠANK1','https://drive.google.com/open?id=1lwu5fu4Y4Qju9S4jT0-MhOpFes6lOxe6'),
+('COCA COLA 0.25L',240.000,15.00,3.09,1.000,'SOKOVI','ŠANK1','https://drive.google.com/open?id=15vY9Xn-_PXikAzKeVFcrIWwuYrYN5BeG'),
 /* 9 */
-('COCKTA 0.25L',200.000,15.00,2.79,1.000,'BEZALKOHOLNA PIĆA','ŠANK1','https://drive.google.com/open?id=1WXphZ8FDe1omP7ab_c2DS-UnPbqUjOmW');
+('FANTA 0.25L',240.000,15.00,3.09,1.000,'SOKOVI','ŠANK1','https://drive.google.com/open?id=1zQP5JnwCbUo9sTgM7OLLlUJ4Dyp2AsN-'),
 /* 10 */
+('SPRITE 0.25L',240.000,15.00,3.09,1.000,'SOKOVI','ŠANK1','https://drive.google.com/open?id=1C6M7rIXE-_WHDmkW1F7TxVMFHuSKjv-l'),
+/* 11 */
+('SCHWEPPES 0.25L',240.000,15.00,3.20,1.000,'SOKOVI','ŠANK1','https://drive.google.com/open?id=1grQiwBmWRgEAQW09L7a2GS9jgyu2j7BM'),
+/* 12 */
+('PRIRODNI SOK CAPPY 0.20L',200.000,15.00,3.50,1.000,'SOKOVI','ŠANK1','https://drive.google.com/open?id=1a8BVXogDk2dQQnXlfK8JlTenLlNxs62J'),
+/* 13 */
+('LEDENI ČAJ 0.20L',240.000,15.00,2.30,1.000,'SOKOVI','ŠANK1','https://drive.google.com/open?id=1mWwYs-avXsl35AD6sKFSMG0malsL3x3Z'),
+/* 14 */
+('ORANGINA 0.25L',240.000,16.00,4.39,1.000,'SOKOVI','ŠANK1','https://drive.google.com/open?id=1jQilEO4pQdYuRzXc0TcpXgXZ9DkFt1xW'),
+/* 15 */
+('CEDEVITA',400.000,11.00,2.11,1.000,'SOKOVI','ŠANK1','https://drive.google.com/open?id=1lwu5fu4Y4Qju9S4jT0-MhOpFes6lOxe6'),
+/* 16 */
+('COCKTA 0.25L',200.000,15.00,2.79,1.000,'SOKOVI','ŠANK1','https://drive.google.com/open?id=1WXphZ8FDe1omP7ab_c2DS-UnPbqUjOmW');
+/* 17 */
 
 /* 10 */
 
