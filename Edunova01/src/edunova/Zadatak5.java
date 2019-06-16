1dbaf734086bee97432b719093b57f4f1c4ef822
CuturaM <50337345+CuturaM@users.noreply.github.com>
2019-06-16 09:47:46 +0200

    Rješenje zadatka

--- Edunova01/src/edunova/Zadatak5.java
+++ Edunova01/src/edunova/Zadatak5.java
@@ -4,12 +4,12 @@ import javax.swing.JOptionPane;
 
 public class Zadatak5 {
 	public static void main(String[] args) {
-		
+
 		boolean Q;
 
 		Q = Boolean.parseBoolean(JOptionPane.showInputDialog("Herbivores eat meat?"));
 
-		System.out.println("Herbivores eat meat?");
+		System.out.println("Herbivores eat meat?[");
 		System.out.println("Answer = " + Q);
 	}
 }
